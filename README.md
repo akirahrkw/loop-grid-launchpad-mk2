cfaf1753457f6c3799978d0d82070e8d858994b7
akirahrkw <akirahrkw@gmail.com>
2016-01-24 22:45:42 +0800

    support launchpad mk2 layout

---
+++ README.md
@@ -0,0 +1,6 @@
+loop-grid-launchpad-mk2
+===
+
+Novation Launchpad mk2 bindings for [loop-grid](https://github.com/mmckegg/loop-grid).
+
+coming soon...
---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'Launchpad MK2',
+  group: 'loop-grids',
+  node: 'controller/launchpad',
+  render: require('../loop-grid/view'),
+  object: require('./object')
+}
